06c828172fdfaec0430e3a1b02bee2ee00f89fdf
Dove Stack <flawal664@gmail.com>
2025-11-16 18:11:01 +0100

    feat: added more custom themes and improved existing ones

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
@@ -18,13 +17,15 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground ",
         link: "text-primary underline-offset-4 hover:underline",
+        ctaBtnOne: "bg-violet-600 text-white hover:bg-violet-700",
+        ctaBtnTwo: "border-solid border-violet-300 text-violet-700 hover:bg-violet-100",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-11 rounded-xl px-8 py-6 font-mono font-bold border-solid border-purple-400 text-violet-500",
         icon: "h-9 w-9",
-        btnNav: "px-6 py-3"
+        btnNav: "px-6 py-3",
       },
     },
     defaultVariants: {
